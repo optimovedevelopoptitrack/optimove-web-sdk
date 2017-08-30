b71d32d22d4ee2174ab86351854b5a8584cc9b1e
Yossi cohn <optimove.integration.optitrack@gmail.com>
2017-08-30 15:07:30 +0300

    add support for event with no parameters

--- public/95671c75b9ec486f687f523a7c37a35d/1.0.0.js
+++ public/95671c75b9ec486f687f523a7c37a35d/1.0.0.js
@@ -229,27 +229,7 @@ var optimoveTenantConfiguration = {
             supportedOnOptitrack: true,
             supportedOnRealTime: true,
             parameters: {
-                action_name: {
-                    optional: false,
-                    name: "action_name",
-                    id: 1,
-                    type: "String",
-                    optiTrackDimensionId: 8
-                },
-                action_value: {
-                    optional: false,
-                    name: "action_value",
-                    id: 2,
-                    type: "Number",
-                    optiTrackDimensionId: 9
-                },
-                action_price: {
-                    optional: false,
-                    name: "action_price",
-                    id: 3,
-                    type: "Number",
-                    optiTrackDimensionId: 10
-                }
+               
             }
         },
         Event5: {
